1da3bf33adfd8150650f9aad31975d45c5ca4251
schemes <salimchemes@gmail.com>
2017-03-20 22:40:00 -0300

    add some basic validation

--- grid.js
+++ grid.js
@@ -1,15 +1,16 @@
 var items = [
-    ['0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
-    ['1', 'Q', '4', '5', '6', '7', '5', 'G', 'H', 'I', 'J', 'K', 'L'],
-    ['2', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', '4'],
-    ['3', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
-    ['4', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
-    ['5', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
-    ['6', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
-    ['7', '1', '3', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+         //0    1    2    3    4    5    6    7    8    9   10    11   12  
+    /*0*/['0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'], /*0*/
+    /*1*/['1', 'Q', 'F', '5', '6', '7', '6', 'G', 'H', 'I', 'J', 'K', 'L'], /*1*/
+    /*2*/['2', 'A', '0', 'C', 'A', 'V', 'B', 'G', 'H', 'I', 'J', 'K', '4'], /*2*/
+    /*3*/['3', 'A', '1', 'C', '6', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'], /*3*/
+    /*4*/['4', 'A', '4', 'C', '4', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'], /*4*/
+    /*5*/['5', 'A', '6', '0', '8', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'], /*5*/
+    /*6*/['6', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'], /*6*/
+    /*7*/['7', '1', '3', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'Z']  /*7*/
 
 ];
-var chords = 'E1A7B7';
+
 function getchord(letter, number) {
     var result;
     for (i = 0; i <= 12; i++) {
@@ -18,18 +19,25 @@ function getchord(letter, number) {
         }
     }
     return result;
+}
+var key = 'A1L2L';
+
+if (key.length != 6) {
+    result = "Some coordinates are missing";
+}
+else {
+    var firstLetter = key.substring(0, 1);
+    var firstNumber = key.substring(1, 2);
+
+    var secondLetter = key.substring(2, 3);
+    var secondNumber = key.substring(3, 4);
+
+    var thirdLetter = key.substring(4, 5);
+    var thirdNumber = key.substring(5, 6);
+    
+    var result = getchord(firstLetter, firstNumber) + getchord(secondLetter, secondNumber) + getchord(thirdLetter, thirdNumber);
+
 
 }
 
-var firstLetter = chords.substring(0, 1);
-var firstNumber = chords.substring(1, 2);
-
-var secondLetter = chords.substring(2, 3);
-var secondNumber = chords.substring(3, 4);
-
-var thirdLetter = chords.substring(4, 5);
-var thirdNumber = chords.substring(5, 6);
-
-var result = getchord(firstLetter, firstNumber) + getchord(secondLetter, secondNumber) + getchord(thirdLetter, thirdNumber);
- 
 console.log(result);
